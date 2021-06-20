48c5d5434b01b1b59c0541b754d2503a9322dfd3
RafaelC4rdoso <rafaelgremista2010@gmail.com>
2021-06-20 15:44:17 -0300

    backend: created rest controllers and added getUserRegister with dao model

---
+++ admin-system/src/main/java/com/betha/login/exception/SqlException.java
@@ -0,0 +1,11 @@
+package com.betha.login.exception;
+
+public class SqlException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public SqlException(String msg) {
+		super(msg);
+	}
+
+}
